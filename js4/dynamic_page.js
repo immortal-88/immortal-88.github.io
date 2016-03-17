5786d4bb808c623106873122c0ab24c72960cff1
Alexander Gusakov <immtherion@gmail.com>
2016-03-17 20:21:59 +0200

    JS 2 task completed

---
+++ js4/dynamic_page.js
@@ -0,0 +1,60 @@
+/**
+ * Created by apple on 17.03.16.
+ */
+
+// DYNAMICALLY CREATED PAGE
+
+var heading = document.createElement('h1');
+heading.className = 'box';
+heading.innerHTML = 'Тесты по программированию';
+heading.style.textAlign = 'center';
+document.body.appendChild(heading);
+
+// Question generation function
+question(3);
+
+//Submit button
+var tab2 = document.createElement('br');
+document.body.appendChild(tab2);
+var submitButton = document.createElement('INPUT');
+submitButton.setAttribute('type', 'submit');
+submitButton.setAttribute('value', 'Проверить мои результаты');
+submitButton.style.padding = '10px';
+submitButton.style.border = '2px solid gray';
+submitButton.style.display = 'block';
+submitButton.style.margin = '20px auto';
+submitButton.style.background = '#81D8D0';
+submitButton.style.color = 'gray';
+document.body.appendChild(submitButton);
+
+//Functions
+function fillCheckbox(number) {
+    for(var i = 1; i <= number; i++) {
+        var checkBox = document.createElement('INPUT');
+        checkBox.setAttribute('type', 'checkbox');
+        checkBox.setAttribute('id',   'answer');
+        checkBox.setAttribute('value', 'a');
+        checkBox.style.marginRight = '20px';
+        var answerLabel = document.createElement('label');
+        answerLabel.setAttribute('for','answer'); // this corresponds to the checkbox id
+        answerLabel.style.marginLeft = '50px';
+        answerLabel.appendChild(checkBox);
+        answerLabel.appendChild(document.createTextNode('Вариант ответа № ' + i));
+        var tab = document.createElement('br');
+        document.body.appendChild(tab);
+        document.body.appendChild(answerLabel);
+    }
+}
+
+function question(number) {
+    for(var i = 1; i <= number; i++) {
+        var subHeading = document.createElement('p');
+        subHeading.className = 'question';
+        subHeading.innerHTML = 'Вопрос № ' + i;
+        subHeading.style.margin = '50px 0 0 80px';
+        subHeading.style.fontWeight = 'bold';
+        subHeading.style.fontSize = '18px';
+        document.body.appendChild(subHeading);
+        fillCheckbox(3);
+    }
+}
