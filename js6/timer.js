2ebeeaf505ab0532143683c1cc80f12a4526643a
Alexander Gusakov <immtherion@gmail.com>
2016-03-28 00:35:18 +0300

    Timer task

--- js6/timer.js
+++ js6/timer.js
@@ -6,11 +6,90 @@
 
 document.body.style.background = "black";
 
+// V A R I A B L E S
 var millis, seconds, minutes, hours, counter;
 millis = seconds = minutes = hours = counter = 0;
-
+var splitCounter = 1;
 var interval = null;
 
+// I N I T I A L I Z I N G   E L E M E N T S
+// Clock face
+var timeText = document.createElement('p');
+timeText.innerHTML = '00:00:00.000';
+timeText.id = 'time-text';
+document.body.appendChild(timeText);
+
+// Buttons
+var buttonBox = document.createElement('div');
+buttonBox.id = 'button-box';
+document.body.appendChild(buttonBox);
+
+// Split button results
+var timeResults = document.createElement('div');
+timeResults.id = 'time-results';
+document.body.appendChild(timeResults);
+
+var startButton = document.createElement('button');
+startButton.className = 'btn btn-success';
+startButton.id = 'start-button';
+startButton.innerHTML = 'Start';
+buttonBox.appendChild(startButton);
+
+var splitButton = document.createElement('button');
+splitButton.className = 'btn btn-warning';
+splitButton.id = 'split-button';
+splitButton.innerHTML = 'Split';
+buttonBox.appendChild(splitButton);
+
+var resetButton = document.createElement('button');
+resetButton.className = 'btn btn-primary';
+resetButton.id = 'reset-button';
+resetButton.innerHTML = 'Reset';
+buttonBox.appendChild(resetButton);
+
+// L I S T E N E R S
+startButton.addEventListener('click', function() {
+    if(startButton.innerHTML == 'Start') {
+        startButton.innerHTML = 'Stop';
+        startButton.className = 'btn btn-danger';
+        startButton.style.transition = "all .6s";
+        // Set interval
+        interval = setInterval(function() {
+            timer();
+        }, 10);
+        splitButton.disabled = false;
+    } else {
+        startButton.innerHTML = 'Start';
+        startButton.className = 'btn btn-success';
+        startButton.style.transition = "all .6s";
+        clearInterval(interval);
+        var stop = document.createElement('p');
+        stop.innerHTML = 'Stopped: ' + timeText.textContent;
+        timeResults.appendChild(stop);
+        splitButton.disabled = true;
+    }
+});
+
+splitButton.addEventListener('click', function() {
+    if(timeText.textContent != '00:00:00.000') {
+        var split = document.createElement('p');
+        split.innerHTML = splitCounter +' lap:' + timeText.textContent;
+        timeResults.appendChild(split);
+        splitCounter++;
+    }
+});
+
+resetButton.addEventListener('click', function() {
+    clearInterval(interval);
+    timeText.innerHTML = '00:00:00.000';
+    timeResults.innerHTML = '';
+    millis = seconds = minutes = hours = counter = 0;
+    startButton.innerHTML = 'Start';
+    startButton.className = 'btn btn-success';
+    splitCounter = 1;
+});
+
+// F U N C T I O N S
 // Timer count
 function timer() {
     var dynamicTime = new Date().getTime();
@@ -48,97 +127,4 @@ function addZero(value, length) {
     }
     return value;
 }
-
-// Clock face
-var timeText = document.createElement('p');
-timeText.innerHTML = '00:00:00.000';
-timeText.style.color ='red';
-timeText.style.fontSize = '60px';
-timeText.style.marginTop = '200px';
-timeText.style.textAlign = 'center';
-timeText.style.fontFamily = 'Digital';
-document.body.appendChild(timeText);
-
-// Buttons
-var buttonBox = document.createElement('div');
-buttonBox.style.width = '350px';
-buttonBox.style.height = '50px';
-buttonBox.style.margin = '0 auto';
-document.body.appendChild(buttonBox);
-
-// Split button results
-var timeResults = document.createElement('div');
-timeResults.style.textAlign = 'center';
-timeResults.style.fontSize = '20px';
-timeResults.style.fontFamily = 'Digital';
-timeResults.style.marginBottom = '20px';
-timeResults.style.color = 'white';
-document.body.appendChild(timeResults);
-
-var startButton = document.createElement('button');
-startButton.className = 'btn btn-success';
-startButton.style.float = 'left';
-startButton.style.textTransform = 'uppercase';
-startButton.style.width = '100px';
-startButton.innerHTML = 'Start';
-buttonBox.appendChild(startButton);
-startButton.addEventListener('click', function() {
-    if(startButton.innerHTML == 'Start') {
-        startButton.innerHTML = 'Stop';
-        startButton.className = 'btn btn-danger';
-        startButton.style.transition = "all .6s";
-        // Set interval
-        interval = setInterval(function() {
-            timer();
-        }, 10);
-        splitButton.disabled = false;
-    } else { // work here
-        startButton.innerHTML = 'Start';
-        startButton.className = 'btn btn-success';
-        startButton.style.transition = "all .6s";
-        clearInterval(interval);
-        var stop = document.createElement('p');
-        stop.innerHTML = 'Stopped: ' + timeText.textContent;
-        timeResults.appendChild(stop);
-        splitButton.disabled = true;
-    }
-});
-
-var splitCounter = 1;
-
-var splitButton = document.createElement('button');
-splitButton.className = 'btn btn-warning';
-splitButton.style.marginLeft = '20px';
-splitButton.style.textTransform = 'uppercase';
-splitButton.style.width = '100px';
-splitButton.style.float = 'left';
-splitButton.innerHTML = 'Split';
-buttonBox.appendChild(splitButton);
-splitButton.addEventListener('click', function() {
-    if(timeText.textContent != '00:00:00.000') {
-        var split = document.createElement('p');
-        split.innerHTML = splitCounter +' lap:' + timeText.textContent;
-        timeResults.appendChild(split);
-        splitCounter++;
-    }
-});
-
-var resetButton = document.createElement('button');
-resetButton.className = 'btn btn-primary';
-resetButton.style.marginLeft = '20px';
-resetButton.style.width = '100px';
-resetButton.style.textTransform = 'uppercase';
-resetButton.style.float = 'left';
-resetButton.innerHTML = 'Reset';
-buttonBox.appendChild(resetButton);
-resetButton.addEventListener('click', function() {
-    clearInterval(interval);
-    timeText.innerHTML = '00:00:00.000';
-    timeResults.innerHTML = '';
-    millis = seconds = minutes = hours = counter = 0;
-    startButton.innerHTML = 'Start';
-    startButton.className = 'btn btn-success';
-    splitCounter = 1;
-});
-
 
