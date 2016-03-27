e97c6682f3e8fbfc8dbb2aa1a8c5274c3f31bc14
Alexander Gusakov <immtherion@gmail.com>
2016-03-28 02:03:49 +0300

    JS first task

--- js6/timer.js
+++ js6/timer.js
@@ -130,6 +130,5 @@ function addZero(value, length) {
 
 document.getElementById('start-button').onclick = (function() {
     document.getElementsByTagName('audio')[0].play();
-    document.getElementsByTagName('span')[0].innerHTML = 'Hello bitchception!';
     return false;
 });
