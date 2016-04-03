2a9a7105d16f350a4bb90e6c60c93be142c4f3ad
Alexander Gusakov <immtherion@gmail.com>
2016-04-03 22:17:06 +0300

    Tabs

---
+++ js8/tabs.js
@@ -0,0 +1,19 @@
+/**
+ * Created by apple on 03.04.16.
+ */
+
+$(document).ready(function() {
+
+    $('.tabs-links li a').on('click', function(e)  {
+        var currentAttrValue = $(this).attr('href');
+
+        // Show/Hide Tabs
+        $('.tabs ' + currentAttrValue).slideDown(400).show().siblings().hide();
+
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+
+});
