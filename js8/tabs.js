2db6972a8dfa3f21a86f2d6dc10e017b79c2d59f
Alexander Gusakov <immtherion@gmail.com>
2016-04-06 16:30:40 +0300

    JS 8

--- js8/tabs.js
+++ js8/tabs.js
@@ -16,14 +16,14 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-    $('input').hover(function() {
-        //var currentText = $(this).attr('title');
-
-        $(this).next().fadeIn(500).show();
-    },
-        function() {
-            $(this).next().fadeOut(500).hide();
+    $('input').focus(function() {
+        if($('input').text =='') {
+            $(this).next().fadeIn(500).show();
         }
-    );
+    });
+
+    $('input').blur(function() {
+        $(this).next().fadeIn(500).hide();
+    });
 
 });
