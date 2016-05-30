c14c1940994d3bd6375a8559ede847ae81024de7
Alexander Gusakov <immtherion@gmail.com>
2016-05-30 15:05:05 +0300

    nothing added

--- js20/app/main.js
+++ js20/app/main.js
@@ -16,4 +16,32 @@ $(function() {
         owl.trigger('to.owl-carousel', [$(this).index(), 300]);
     });
 
+//    L O D A S H   T E S T
+
+    // 1 task
+    var skills = json.map(function(obj) {
+        return obj.skills;
+    });
+
+    var arr1 = _.sortedUniq(_.sortBy(_.flatten(skills)));
+    console.log('Задание 1', arr1);
+
+    // 2 task
+    //var names = json.map(function(obj) {
+    //    return obj.name;
+    //});
+    //
+    //json.forEach(function(item) {
+    //    console.log(_.sortBy(names, item.friends));
+    //});
+
+    //console.log(names);
+
+    // 3 task
+    var friends = json.map(function(obj) {
+       return obj.friends;
+    });
+
+    console.log((_.flatten(friends)));
+
 });
